dbb8d9c3da87baa7217ea876eccaad35cb10db5a
NicYeow <nicholas_yeow@mymail.sutd.edu.sg>
2017-05-16 20:36:42 +0800

    Fixed TestFireBase File

--- src/Server/TestFireBase.java
+++ src/Server/TestFireBase.java
@@ -3,38 +3,41 @@ package Server;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseCredentials;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.*;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 /**
  * Created by nicholas on 15-May-17.
  */
 public class TestFireBase {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // Fetch the service account key JSON file contents
         FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccountCredentials.json");
 
 // Initialize the app with a service account, granting admin privileges
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredential(FirebaseCredentials.fromCertificate(serviceAccount))
+                .setDatabaseUrl("https://databaseName.firebaseio.com")
+                .build();
+        FirebaseApp.initializeApp(options);
+
 // As an admin, the app has access to read and write all data, regardless of Security Rules
         DatabaseReference ref = FirebaseDatabase
                 .getInstance()
                 .getReference("restricted_access/secret_document");
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredential(FirebaseCredentials.fromCertificate(serviceAccount))
-                    .setDatabaseUrl("https://databaseName.firebaseio.com")
-                    .build();
-        FirebaseApp.initializeApp(options);
-
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Object document = dataSnapshot.getValue();
                 System.out.println(document);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }

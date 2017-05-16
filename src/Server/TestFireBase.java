1feac37930e783923e1520d199d7950d554b37b3
NicYeow <nicholas_yeow@mymail.sutd.edu.sg>
2017-05-16 20:31:19 +0800

    Removed workspace from tracked files

--- src/Server/TestFireBase.java
+++ src/Server/TestFireBase.java
@@ -20,18 +20,18 @@ public class TestFireBase {
         FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccountCredentials.json");
 
 // Initialize the app with a service account, granting admin privileges
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredential(FirebaseCredentials.fromCertificate(serviceAccount))
-                .setDatabaseUrl("https://databaseName.firebaseio.com")
-                .build();
-        FirebaseApp.initializeApp(options);
-
 // As an admin, the app has access to read and write all data, regardless of Security Rules
         DatabaseReference ref = FirebaseDatabase
                 .getInstance()
                 .getReference("restricted_access/secret_document");
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredential(FirebaseCredentials.fromCertificate(serviceAccount))
+                    .setDatabaseUrl("https://databaseName.firebaseio.com")
+                    .build();
+        FirebaseApp.initializeApp(options);
+
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Object document = dataSnapshot.getValue();
                 System.out.println(document);

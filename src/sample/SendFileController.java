e53f34f0e322f04546195035d530310048e36f27
NicYeow <nicholas_yeow@mymail.sutd.edu.sg>
2017-05-18 20:54:42 +0800

    Initial commit after first reset to commit : d39e1787ffd252c6c218c8f74b8d0c526b50fb10

---
+++ src/sample/SendFileController.java
@@ -0,0 +1,88 @@
+package sample;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class SendFileController implements Initializable {
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private JFXButton homeButton;
+
+    @FXML
+    private JFXListView<Label> fileListView;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+//        this.listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Label>() {
+        homeButton.setOnAction(e -> {
+            try {
+                goHome();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+
+    }
+    @FXML
+    private void handleDragOver(DragEvent event){
+        //got the plus sign when you hover over it
+        //only if it is a file, not html etc
+        if(event.getDragboard().hasFiles()){
+            event.acceptTransferModes(TransferMode.ANY);
+        }
+
+    }
+    @FXML
+    private void handleDrop(DragEvent event) throws FileNotFoundException {
+        System.out.println("something dropped");
+        List<File> files = event.getDragboard().getFiles();
+        String fileName = files.get(0).getName();
+        Image img = new Image(new FileInputStream(files.get(0)));
+//        ObservableList<Label> fileList;
+//        fileList = this.fileListView.getSelectionModel().getSelectedItems();
+//        for (Label l: fileList){
+//            System.out.println(l);
+//        }
+        Label lbl = new Label("File Name: " + fileName);
+        this.fileListView.getItems().add(lbl);
+        // the code below can change the image
+//        this.imageView.setImage(img);
+    }
+    private void goHome() throws IOException {
+        Parent send_file_page_parent = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Scene send_file_page_scene = new Scene(send_file_page_parent);
+        Stage app_stage = (Stage) this.homeButton.getScene().getWindow();
+        app_stage.setScene(send_file_page_scene);
+        app_stage.show();
+    }
+
+
+}
